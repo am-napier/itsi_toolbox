f559857a1eb7e24525d332d52e20b665d3b6b193
am-napier <anapier@splunk.com>
2022-04-02 08:44:56 +0100

    starting on delete objects command

--- bin/command_delete_objects.py
+++ bin/command_delete_objects.py
@@ -8,14 +8,14 @@ import json
 import time
 
 @Configuration()
-class RMEntityCommand(StreamingCommand):
+class DeleteObjectsCommand(StreamingCommand):
 
     """
     Option not supported by the REST API so commented for now
     opt_batch_size = Option(
         doc='''
         **Syntax:** **batch_size=***int*
-        **Description:** Number of entities to delete per operation.
+        **Description:** Number of objects to delete per operation.
         **Default:** 100''',
         name='batch_size',
         require=False,
@@ -63,9 +63,20 @@ class RMEntityCommand(StreamingCommand):
         default=False,
         validate=validators.Boolean())
 
+    opt_type = Option(
+        doc='''
+        **Syntax:** **type=***entity|service|kpi_threshold_template|deepdive*
+        **Description:** String value from the REST endpoint itoa_interface/get_supported_object_types
+        **Default:** entity''',
+        name='type',
+        require=False,
+        default="entity",
+        
+        validate=validators.Set())    
+
 
     def __init__(self):
-        super(RMEntityCommand, self).__init__()
+        super(DeleteObjectsCommand, self).__init__()
 
 
     def prepare(self):
@@ -82,7 +93,7 @@ class RMEntityCommand(StreamingCommand):
         n = 0
         for record in records:
             n = n+1
-            self.logger.debug('Record to delete entities: '.format(record))
+            self.logger.debug('Record to delete ITSI Objects: '.format(record))
             t2 = time.time()
             if self.opt_by_id:
                 self.delete_by_id(record)
@@ -100,7 +111,7 @@ class RMEntityCommand(StreamingCommand):
 
     def get_context(self):
         if not hasattr(self, 'context'):
-            self.logger.debug("rmentity prepare, creating context")
+            self.logger.debug("delitsi prepare, creating context")
             self.context = splunklib.binding.connect(token=self.service.token, host="localhost", port=8089)
 
         return self.context
@@ -109,7 +120,7 @@ class RMEntityCommand(StreamingCommand):
     def delete_by_id(self, record):
         entity_id=record.get("id", "None")
         self.logger.info("called delete_by_id:{}".format(entity_id))
-        url = "/servicesNS/nobody/SA-ITOA/itoa_interface/entity/%s" % entity_id
+        url = "/servicesNS/nobody/SA-ITOA/itoa_interface/%s/%s" % self.opt_type, entity_id
         res = self.get_context().delete(url)
         self.logger.info("Delete Response status:{}, ".format(res['status']))
         record['response'] = res['status']
@@ -126,80 +137,25 @@ class RMEntityCommand(StreamingCommand):
         value = '"%s"' % value
         if is_rex: #
             value = '{"$regex": %s}' % value
-        entity_filter = '{"%s": %s}' % (key, value)
-        url = "/servicesNS/nobody/SA-ITOA/itoa_interface/entity"
+        object_filter = '{"%s": %s}' % (key, value)
+        url = "/servicesNS/nobody/SA-ITOA/itoa_interface/%s" % self.opt_type
         if dry_run:
-            res = self.get_context().get(url, filter=entity_filter)
-            record['filter-string'] = entity_filter
+            res = self.get_context().get(url, filter=object_filter)
+            record['filter-string'] = object_filter
             record['filter-response'] = res['status']
             record['filter-count'] = len(json.loads(res['body'].read()))
         else:
-            self.logger.info("Entity delete filter is %s" % (entity_filter))
-            res = self.get_context().delete(url, filter=entity_filter)
+            self.logger.info("Entity delete filter is %s" % (object_filter))
+            res = self.get_context().delete(url, filter=object_filter)
             if int(res['status']) > 299:
                 record['filter-response'] = "Error during entity delete {}".format(res['status'])
                 self.logger.error(record['filter-response'])
             else:
-                record['filter-response'] = "Entity delete OK status:{}".format(res['status'])
+                record['filter-response'] = "ITSI Object delete OK status:{}".format(res['status'])
                 self.logger.info(record['filter-response'])
 
         self.logger.info("Delete Response status:{}, ".format(record))
 
 
-    def future_get_ids(self, entity_filter, sz):
-        '''
-        find all the id that match this filter
-        '''
-        ids = []
-        url = "/servicesNS/nobody/SA-ITOA/itoa_interface/entity"
-        res = self.get_context().get(url, filter=entity_filter, fields="title")
-        for j in json.loads(res['body'].read()):
-            ids.append(j['_key'])
-        return ids
-
-
-    def future_delete_by_filter(self, is_rex, sz, dry_run, record):
-        '''
-        filter='{"title": "foo"}'
-          or
-        filter='{"type": {"$regex":"(od|eve).*"}}
-        '''
-        key = record.get("filter_key", "KEY-NotSp3cIf13D")
-        value = record.get("filter_value", "VALUE-NotSp3cIf13D")
-        sz = max(sz, 2)
-        self.logger.info("delete by filter key: {}, value:{}, is_rex:{}, sz:{} ".format(key, value, is_rex, sz))
-        value = '"%s"' % value
-        if is_rex:
-            value = '{"$regex": %s}' % value
-        entity_filter = '{"%s": %s}' % (key, value)
-        url = "/servicesNS/nobody/SA-ITOA/itoa_interface/entity"
-        if dry_run:
-            res = self.get_context().get(url, filter=entity_filter)
-            record['filter-string'] = entity_filter
-            record['filter-response'] = res['status']
-            record['filter-count'] = len(json.loads(res['body'].read()))
-        else:
-            n=1
-            while time.time() < self.timeout:
-                self.logger.info("Pass %d, Entity delete filter is %s" % (n, entity_filter))
-                n = n+1
-                res = self.get_context().delete(url, limit=sz, filter=entity_filter)
-                if int(res['status']) > 299:
-                    record['filter-response'] = "Error during entity delete/delete {}".format(res['status'])
-                    self.logger.error(record['filter-response'])
-                    break
-                res = self.get_context().get(url, filter=entity_filter)
-                if int(res['status']) > 299:
-                    record['filter-response'] = "Error during entity delete/count {}".format(res['status'])
-                    self.logger.error(record['filter-response'])
-                    break
-                remaining = len(json.loads(res['body'].read()))
-                if remaining == 0:
-                    self.logger.info("All entities deleted")
-                    break
-                self.logger.info("%d entities remain, looping on delete" % remaining)
-
-        self.logger.info("Delete Response status:{}, ".format(record))
-
 if __name__ == '__main__':
-    dispatch(RMEntityCommand, module_name=__name__)
+    dispatch(DeleteObjectsCommand, module_name=__name__)
---
+++ bin/test/helloworld.py
@@ -0,0 +1,6 @@
+print("hello, world!")
+
+a = 1+2
+
+print(a)
+
